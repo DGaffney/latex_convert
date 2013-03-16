60b618a19f809607926c9a7e9c571737d44a26c9
Devin Gaffney <itsme@devingaffney.com>
2013-03-15 17:40:22 -0700

    internet

--- app.rb
+++ app.rb
@@ -59,6 +59,10 @@ class LatexConvert < Sinatra::Base
       tmp_dir = "public/files/finished_#{random_name}"
       `mkdir -p #{tmp_dir}`
       texable_files.each do |tex_file|
+        tex_file = File.read("#{new_dir}/#{tex_file}").gsub!(".eps}", "}")
+        f = File.open("#{new_dir}/#{tex_file}", "w")
+        f.write(tex_file)
+        f.close
         `rubber -d --into #{tmp_dir} #{new_dir}/#{tex_file}`
       end
       `zip -rj9 #{tmp_dir}.zip #{tmp_dir}`
